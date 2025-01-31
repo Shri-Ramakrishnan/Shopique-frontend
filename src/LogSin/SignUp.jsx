c5d10fa8b7fd86312fa05d144119d66a6847d395
Shri Ramakrishnan <ramvaisu.m@gmail.com>
2025-01-31 09:24:54 +0530

    commit

--- src/LogSin/SignUp.jsx
+++ src/LogSin/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./SignUp.css"; 
+import "./Signup.css"; 
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
